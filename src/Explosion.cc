5a86d9271819321576cba488ab429765c66a4436
bernatcuixart <bernat.cuixart@estudiantat.upc.edu>
2026-08-18 20:31:52 +0200

    Follow-up fixes from mobile version

--- src/Explosion.cc
+++ src/Explosion.cc
@@ -6,14 +6,14 @@ Explosion::Explosion(const Vector2 p, const float r, const float s) :
 {
 	_elapsedLifetime = 0;
 
-	for (int i = 0; i < EXPLOSION_PROPS_NUMBER; ++i) 
+	for (int i = 0; i < EXPLOSION_PROPS_NUMBER; ++i)
 	{
-		_propsEnabled[i] = GetRandomValue(0,1) == 0;
+		_propsEnabled[i] = GetRandomValue(0, 1) == 0;
 		_propsPositions[i] = _position;
 		_propsVelocities[i] = { float(GetRandomValue(-600, 600)), float(GetRandomValue(-200, -1000)) };
 		_propsRotations[i] = float(GetRandomValue(0, 360));
 		_propsRotationSpeeds[i] = float(GetRandomValue(-180, 180));
-	} 
+	}
 
 	_explosionSound = GameManager::instance->audioManager->GetBombExplosionSound();
 	SetSoundVolume(_explosionSound, GameManager::instance->GetBombExplosionSoundVolume());
@@ -29,12 +29,12 @@ Explosion::~Explosion()
 }
 
 void Explosion::Update(float deltaTime)
-{	
+{
 	_elapsedLifetime += deltaTime;
 
-	for (int i = 0; i < EXPLOSION_PROPS_NUMBER; ++i) 
+	for (int i = 0; i < EXPLOSION_PROPS_NUMBER; ++i)
 	{
-		if (_propsEnabled[i]) 
+		if (_propsEnabled[i])
 		{
 			_propsPositions[i].x += _propsVelocities[i].x * deltaTime;
 			_propsPositions[i].y += _propsVelocities[i].y * deltaTime;
@@ -45,7 +45,7 @@ void Explosion::Update(float deltaTime)
 	}
 
 	// Twice as long for the sound to play out, since the sound is longer than the visual effect
-	if (_elapsedLifetime >= EXPLOSION_DURATION * 2.0f) 
+	if (_elapsedLifetime >= EXPLOSION_DURATION * 2.0f)
 	{
 		GameManager::instance->DestroyExplosion(this);
 	}
@@ -55,7 +55,7 @@ void Explosion::Render(const float deltaTime)
 {
 	Rectangle dest = { _position.x, _position.y, _scale, _scale };
 	Vector2 origin = { _scale / 2, _scale / 2 };
-	
+
 	float animProgress = (_elapsedLifetime / EXPLOSION_DURATION);
 	animProgress = fminf(animProgress, 1.0f);
 
@@ -63,9 +63,9 @@ void Explosion::Render(const float deltaTime)
 	Vector2 propOrigin = { propSize / 2, propSize / 2 };
 	float propAlpha = fmaxf(0.0f, 1.0f - animProgress * 2.0f);
 
-	for (int i = 0; i < EXPLOSION_PROPS_NUMBER; ++i) 
+	for (int i = 0; i < EXPLOSION_PROPS_NUMBER; ++i)
 	{
-		if (_propsEnabled[i]) 
+		if (_propsEnabled[i])
 		{
 			DrawTexturePro
 			(GameManager::instance->sprExplosionProps,
@@ -76,10 +76,17 @@ void Explosion::Render(const float deltaTime)
 		}
 	}
 
-	DrawTexturePro
-	(GameManager::instance->sprExplosion,
-		{ float(int(animProgress * EXPLOSION_ANIMATION_SPRITES) * EXPLOSION_SPRITE_SIZE), 0, EXPLOSION_SPRITE_SIZE, EXPLOSION_SPRITE_SIZE }, // SOURCE
+	BeginBlendMode(BLEND_ADDITIVE);
+	int frame = int(animProgress * EXPLOSION_ANIMATION_SPRITES);
+
+	float srcX = float(frame % 16) * EXPLOSION_SPRITE_SIZE;
+	float srcY = float(frame / 16) * EXPLOSION_SPRITE_SIZE;
+
+	DrawTexturePro(
+		GameManager::instance->sprExplosion,
+		{ srcX, srcY, EXPLOSION_SPRITE_SIZE, EXPLOSION_SPRITE_SIZE },
 		dest, origin, 0, WHITE
 	);
-	
+	EndBlendMode();
+	if (GameManager::instance->sprExplosion.id == 0) DrawRectangle(0, 0, 1000, 1000, RED);
 }
